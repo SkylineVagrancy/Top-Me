18c76afdba828cbb30b586469431945643cbf4a4
周俊鹏 <854350704@qq.com>
2017-03-02 09:31:10 +0800

    swift开始

--- Top Me/ViewController.swift
+++ Top Me/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -18,24 +19,57 @@ class ViewController: UIViewController {
     var seconds = 0
     var timer = Timer()
     
+    var buttonBeep : AVAudioPlayer?
+    var secondBeep : AVAudioPlayer?
+    var backgroundMusic : AVAudioPlayer?
+    
+    func setupAudioPlayerWithFile(file:NSString,type:NSString) ->  AVAudioPlayer? {
+        let path=Bundle.main.path(forResource: file as String, ofType: type as String)
+        let url=NSURL.fileURL(withPath: path!)
+        
+        var audioPlayer : AVAudioPlayer?
+        do{
+            try audioPlayer = AVAudioPlayer(contentsOf: url)
+        }catch{
+            print(" player is not available")
+        }
+        
+        return audioPlayer
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let buttonBeep = self.setupAudioPlayerWithFile(file: "ButtonTap", type: "wav"){
+            self.buttonBeep = buttonBeep;
+        }
+        if let secondBeep = self.setupAudioPlayerWithFile(file: "SecondBeep", type: "wav"){
+            self.secondBeep = secondBeep;
+        }
+        if let backgroundMusic = self.setupAudioPlayerWithFile(file: "HallOfTheMountainKing", type: "mp3"){
+            self.backgroundMusic = backgroundMusic;
+        }
+        
         setupGame()
+        view.backgroundColor = UIColor(patternImage: UIImage(named:"bg_tile.png")!)
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func buttonPressed(){
+        buttonBeep?.play()
         count+=1
         scoreLable.text = "Score \n\(count)"
     }
     
     func setupGame(){
+        backgroundMusic?.volume = 0.3
+        backgroundMusic?.play()
         seconds = 30
         count=0
         timeLable.text = "Time: \(seconds)"
@@ -43,6 +77,7 @@ class ViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 1.0,target: self ,selector: #selector(ViewController.subtractTimer),userInfo: nil,repeats: true)
     }
     func subtractTimer(){
+        secondBeep?.play()
         seconds -= 1
         timeLable.text = "Time: \(seconds)"
         if(seconds==0){
